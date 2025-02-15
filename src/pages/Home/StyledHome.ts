21bf89ca86d90bdf271b2878bf6519eb2292324c
MeninoDenii <zapnot14@gmail.com>
2025-02-14 22:55:49 -0300

    feat: initialized homePage integration

--- src/pages/Home/StyledHome.ts
+++ src/pages/Home/StyledHome.ts
@@ -1,3 +1,87 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 0 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+  font-family: "Archivo", sans-serif;
+  overflow-x: hidden;
+
+  @media (max-width: 480px) {
+    display: none;
+  }
+`;
+
+export const Th = styled.th`
+  background: #282c34;
+  color: white;
+  padding: 10px;
+  text-align: left;
+  border: 1px solid #444;
+`;
+
+export const Td = styled.td`
+  padding: 16px;
+  border: 1px solid #ccc;
+  text-align: left;
+`;
+
+export const Tr = styled.tr`
+  &:nth-child(even) {
+    background: #f2f2f2;
+  }
+`;
+
+export const Mobile = styled.div`
+  display: none;
+  @media (max-width: 480px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+  }
+`;
+
+export const Card = styled.div`
+  width: 340px;
+  border: 1px solid #dedede;
+  border-radius: 16px;
+  display: flex;
+  font-family: "Archivo", sans-serif;
+  flex-direction: column;
+`;
+
+export const HeaderCard = styled.header`
+  padding: 16px 24px;
+  border-bottom: 1px solid #dedede;
+`;
+
+export const TitleCard = styled.h1`
+  font-size: 16px;
+`;
+
+export const ContentCard = styled.div`
+  padding: 16px 24px;
+`;
+
+export const InfoCard = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const InfoTitle = styled.span`
+  font-size: 16px;
+  color: #656a6e;
+`;
+export const InfoValue = styled.span`
+  color: #222529;
+`;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,131 @@
+import { useEffect, useState } from "react";
+import {
+  Container,
+  Table,
+  Td,
+  Th,
+  Tr,
+  Mobile,
+  Card,
+  ContentCard,
+  HeaderCard,
+  InfoCard,
+  InfoTitle,
+  InfoValue,
+  TitleCard,
+} from "./StyledHome";
+import { toast, ToastContainer } from "react-toastify";
+
+interface iPlanets {
+  next: string;
+  results: {
+    name: string;
+    rotation_period: string;
+    orbital_period: string;
+    diameter: string;
+    climate: string;
+    gravity: string;
+    terrain: string;
+    surface_water: string;
+    population: string;
+    residents: string[];
+    films: string[];
+    created: string;
+    edited: string;
+    url: string;
+  }[];
+}
+
 const HomePage = () => {
-  return <h1>Home page</h1>;
+  const [planets, setPlanets] = useState<iPlanets["results"]>();
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/planets/")
+      .then((response) => response.json())
+      .then((data: iPlanets) => {
+        setPlanets(data.results);
+      })
+      .catch(() => {
+        toast.error("Erro ao buscar os dados dos planetas");
+      });
+  }, []);
+
+  return (
+    <>
+      <ToastContainer />
+      <Container>
+        <Table>
+          <thead>
+            <tr>
+              <Th>Nome</Th>
+              <Th>Período de rotação</Th>
+              <Th>Período de orbita</Th>
+              <Th>Diâmetro</Th>
+              <Th>Clima</Th>
+              <Th>Gravidade</Th>
+              <Th>Terreno</Th>
+              <Th>Água Superficial</Th>
+              <Th>População</Th>
+            </tr>
+          </thead>
+          <tbody>
+            {planets?.map((planet) => (
+              <Tr key={planet.name}>
+                <Td>{planet.name}</Td>
+                <Td>{planet.rotation_period}</Td>
+                <Td>{planet.orbital_period}</Td>
+                <Td>{planet.diameter}</Td>
+                <Td>{planet.climate}</Td>
+                <Td>{planet.gravity}</Td>
+                <Td>{planet.terrain}</Td>
+                <Td>{planet.surface_water}</Td>
+                <Td>{planet.population}</Td>
+              </Tr>
+            ))}
+          </tbody>
+        </Table>
+        <Mobile>
+          {planets?.map((planet) => (
+            <Card key={planet.name}>
+              <HeaderCard>
+                <TitleCard>{planet?.name}</TitleCard>
+              </HeaderCard>
+              <ContentCard>
+                <InfoCard>
+                  <InfoTitle>Período de rotação:</InfoTitle>
+                  <InfoValue>{planet?.rotation_period}</InfoValue>
+                </InfoCard>
+                <InfoCard>
+                  <InfoTitle>Período de Órbita:</InfoTitle>
+                  <InfoValue>{planet?.orbital_period}</InfoValue>
+                </InfoCard>
+                <InfoCard>
+                  <InfoTitle>Diâmetro:</InfoTitle>
+                  <InfoValue>{planet?.diameter}</InfoValue>
+                </InfoCard>
+                <InfoCard>
+                  <InfoTitle>Clima:</InfoTitle>
+                  <InfoValue>{planet?.climate}</InfoValue>
+                </InfoCard>
+                <InfoCard>
+                  <InfoTitle>Gravidade:</InfoTitle>
+                  <InfoValue>{planet?.gravity}</InfoValue>
+                </InfoCard>
+                <InfoCard>
+                  <InfoTitle>Terreno:</InfoTitle>
+                  <InfoValue>{planet?.terrain}</InfoValue>
+                </InfoCard>
+                <InfoCard>
+                  <InfoTitle>População:</InfoTitle>
+                  <InfoValue>{planet?.population}</InfoValue>
+                </InfoCard>
+              </ContentCard>
+            </Card>
+          ))}
+        </Mobile>
+      </Container>
+    </>
+  );
 };
 
 export default HomePage;
